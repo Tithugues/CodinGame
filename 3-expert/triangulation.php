3f9df62da419732f2f350974471d866c91a23b13
Hugues Peccatte <hugues.peccatte@gmail.com>
2015-03-22 22:43:59 +0100

    Triangulation: improve next point search.
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- 3-expert/triangulation.php
+++ 3-expert/triangulation.php
@@ -8,6 +8,56 @@ function _d($var, $force = false) {
     }
 }
 
+class Fx {
+    /** @var float */
+    protected $_a = null;
+    /** @var float */
+    protected $_b = null;
+
+    /**
+     * Construct a f(x)
+     * @param $a
+     * @param $b
+     */
+    public function __construct($a, $b) {
+        $this->_a = (float)$a;
+        $this->_b = (float)$b;
+    }
+
+    /**
+     * Get "a" constant
+     * @return float
+     */
+    public function getA() {
+        return $this->_a;
+    }
+
+    public function getB() {
+        return $this->_b;
+    }
+
+    public function getY($x) {
+        return $this->getA() * $x + $this->getB();
+    }
+
+    public function getXFromY($y) {
+        return ($y - $this->getB()) / $this->getA();
+    }
+
+    /**
+     * Get X of crossing point
+     * @param Fx $f
+     * @return bool|float X of crossing point or false if parallels
+     */
+    public function getCrossingX(Fx $f) {
+        if ($this->getA() == $f->getA()) {
+            return false;
+        }
+
+        return ($this->getB() - $f->getB()) / ($this->getA() - $f->getA());
+    }
+}
+
 class Map {
     const UP = 0;
     const DOWN = 1;
@@ -56,43 +106,49 @@ class Map {
         $minX = min($xKeys);
         $maxX = max($xKeys);
 
-        if (0 < $this->_firstMoves--) {
-            $minYForMinX = $this->_currentY;
-            $maxYForMinX = $this->_currentY;
-            $minYForMaxX = $this->_currentY;
-            $maxYForMaxX = $this->_currentY;
-        } else {
-            $yKeysForMinX = array_keys($this->_map[$minX]);
-            $minYForMinX = min($yKeysForMinX);
-            $maxYForMinX = max($yKeysForMinX);
-            $yKeysForMaxX = array_keys($this->_map[$maxX]);
-            $minYForMaxX = min($yKeysForMaxX);
-            $maxYForMaxX = max($yKeysForMaxX);
+        $medianeYForMinX = ($this->_map[$minX]['min'] + $this->_map[$minX]['max']) / 2;
+        $medianeYForMaxX = ($this->_map[$maxX]['min'] + $this->_map[$maxX]['max']) / 2;
+
+        //Get f(x) for the mediane.
+        $fMediane = $this->_getFx($minX, $medianeYForMinX, $maxX, $medianeYForMaxX);
+
+        //If mediane is vertical...
+        if (false === $fMediane) {
+            $medianeX = round(($minX + $maxX) / 2);
+            $x = $medianeX + ($medianeX - $this->_currentX);
+            $y = $this->_currentY;
+
+            if (true === $move) {
+                $this->move($x, $y);
+            }
+
+            return array($x, $y);
         }
 
-        $dists = array();
-        $dists[] = $distMinXMinY = pow($this->_currentX - $minX, 2) + pow($this->_currentY - $minYForMinX, 2);
-        $dists[] = $distMinXMaxY = pow($this->_currentX - $minX, 2) + pow($this->_currentY - $maxYForMinX, 2);
-        $dists[] = $distMaxXMinY = pow($this->_currentX - $maxX, 2) + pow($this->_currentY - $minYForMaxX, 2);
-        $dists[] = $distMaxXMaxY = pow($this->_currentX - $maxX, 2) + pow($this->_currentY - $maxYForMaxX, 2);
-        $maxDist = max($dists);
+        //Get f(x) perpendicular to this mediane, going through current position. Generate new X Y.
+        $fPerpendicular = $this->_getFPerpendicular(
+            $minX, $medianeYForMinX, $maxX, $medianeYForMaxX, $this->_currentX, $this->_currentY
+        );
 
-        if ($distMinXMinY === $maxDist) {
-            $x = $minX;
-            $y = $minYForMinX;
-        } elseif ($distMinXMaxY === $maxDist) {
-            $x = $minX;
-            $y = $maxYForMinX;
-        } elseif ($distMaxXMinY === $maxDist) {
-            $x = $maxX;
-            $y = $minYForMaxX;
-        } elseif ($distMaxXMaxY === $maxDist) {
-            $x = $maxX;
-            $y = $maxYForMaxX;
-        } else {
-            _d($dists);
-            die('ERROR line ' . __LINE__ . ': ' . $maxDist);
+        //If mediane is horizontal, perpendicular should be vertical.
+        if (false === $fPerpendicular) {
+            $x = $this->_currentX;
+
+            $medianeY = round(($medianeYForMinX + $medianeYForMaxX) / 2);
+            $y = $medianeY + ($medianeY - $this->_currentY);
+
+            if (true === $move) {
+                $this->move($x, $y);
+            }
+
+            return array($x, $y);
         }
+
+        //Find place of new point. We know current point. We should go the point
+        $crossX = $fMediane->getCrossingX($fPerpendicular);
+
+        $x = round($crossX + ($crossX - $this->_currentX));
+        $y = round($fPerpendicular->getY($x));
 
         if (true === $move) {
             $this->move($x, $y);
@@ -116,14 +172,13 @@ class Map {
             return;
         }
 
-        $moveX = $this->_currentX - $this->_previousX;
-        $moveY = $this->_currentY - $this->_previousY;
+        $f = $this->_getFSeparation($this->_previousX, $this->_previousY, $this->_currentX, $this->_currentY);
 
         //We know that the movement is from X cases and Y cases.
         //Let's find the limit line between these 2 places.
 
         //If horizontal move, split vertically.
-        if (0 === $moveY) {
+        if (false === $f) {
             $mediane = ($this->_currentX + $this->_previousX) / 2;
             if ('COLDER' === $far) {
                 $toRemove = ($this->_currentX > $this->_previousX ? self::RIGHT : self::LEFT);
@@ -138,54 +193,9 @@ class Map {
             return;
         }
 
-        /***********************
-        //This part is a specific case (vertical move) of generic case, with $a = 0;
-        //Check if this is more efficient.
-
-        //If vertical move, split horizontally.
-        if (0 === $moveX) {
-            $mediane = ($this->_currentY + $this->_previousY) / 2;
-            if ('COLDER' === $far) {
-                $toRemove = ($this->_currentY > $this->_previousY ? self::UP : self::DOWN);
-            } elseif ('WARMER' === $far) {
-                $toRemove = ($this->_currentY < $this->_previousY ? self::UP : self::DOWN);
-            } elseif ('SAME' === $far) {
-                $toRemove = self::NOT_LIMIT;
-            } else {
-                die('ERROR line ' . __LINE__ . ': ' . $far);
-            }
-            $this->_eraseDataVerticalMove($mediane, $toRemove);
-            return;
-        }
-
-        /**********************/
-
-        $tmp = $moveX;
-        $moveX = $moveY;
-        $moveY = $tmp;
-        unset($tmp);
-
-        //look for f(x) = ax + b
-        if (0 === $moveX) {
-            $a = 0;
-        } else {
-            $a = -$moveY / $moveX;
-        }
-
-        //Center of move from old place to new place.
-        $centerX = ($this->_currentX + $this->_previousX) / 2;
-        $centerY = ($this->_currentY + $this->_previousY) / 2;
-
-        $b = $centerY - ($a * $centerX);
-
-        $f = $this->_getF($a, $b);
-
-        _d('$centerY: ' . $centerY);
-        _d('$a: ' . $a);
-        _d('$centerX: ' . $centerX);
-        _d('$b: ' . $b);
-        _d('$f($centerX): ' . $f($centerX));
-        _d('$f(2): ' . $f(2));
+        _d('$a: ' . $f->getA());
+        _d('$b: ' . $f->getB());
+        _d('$f(2): ' . $f->getY(2));
 
         if ('SAME' === $far) {
             $toRemove = self::NOT_LIMIT;
@@ -200,12 +210,95 @@ class Map {
         $this->_eraseData($f, $toRemove);
     }
 
-    protected function _getF($a, $b) {
-        return function($x) use ($a, $b) {
-            return $a * $x + $b;
-        };
+    /**
+     * Generate Fx from a and b
+     * @param int $a
+     * @param int $b
+     * @return Fx
+     */
+    protected function _generateFx($a, $b) {
+        return new Fx($a, $b);
     }
 
+    /**
+     * Get f(x) from 2 points
+     * @param int $x1 X first point
+     * @param int $y1 Y first point
+     * @param int $x2 X second point
+     * @param int $y2 Y second point
+     * @return bool|Fx f(x) or false if perpendicular is vertical
+     */
+    protected function _getFx($x1, $y1, $x2, $y2) {
+        $moveY = $y2 - $y1;
+
+        //If horizontal move, split vertically. No f(x) possible.
+        if (0 === $moveY) {
+            return false;
+        }
+
+        $moveX = $x2 - $x1;
+
+        //look for f(x) = ax + b
+        $a = -$moveY / $moveX;
+        $b = $y1 - ($a * $x1);
+
+        return $this->_generateFx($a, $b);
+    }
+
+    /**
+     * Get perpendicular line between 2 points, crossing one specific point.
+     * @param int $x1 X first point
+     * @param int $y1 Y first point
+     * @param int $x2 X second point
+     * @param int $y2 Y second point
+     * @param int $crossX X point to cross
+     * @param int $crossY Y point to cross
+     * @return bool|Fx f(x) or false if perpendicular is vertical
+     */
+    protected function _getFPerpendicular($x1, $y1, $x2, $y2, $crossX, $crossY) {
+        $moveY = $y2 - $y1;
+
+        //If horizontal move, split vertically. No f(x) possible.
+        if (0 == $moveY) {
+            return false;
+        }
+
+        $moveX = $x2 - $x1;
+
+        $tmp = $moveX;
+        $moveX = $moveY;
+        $moveY = $tmp;
+        unset($tmp);
+
+        //look for f(x) = ax + b
+        $a = -$moveY / $moveX;
+        $b = $crossY - ($a * $crossX);
+
+        return $this->_generateFx($a, $b);
+    }
+
+    /**
+     * Get separation line between 2 points.
+     * Unlike perpendicular, separation is going through the center of the 2 points.
+     * @param int $x1 X first point
+     * @param int $y1 Y first point
+     * @param int $x2 X second point
+     * @param int $y2 Y second point
+     * @return bool|Fx f(x) or false if perpendicular is vertical
+     */
+    protected function _getFSeparation($x1, $y1, $x2, $y2) {
+        //Center of move from old point to new point.
+        $centerX = ($x1 + $x2) / 2;
+        $centerY = ($y1 + $y2) / 2;
+
+        return $this->_getFPerpendicular($x1, $y1, $x2, $y2, $centerX, $centerY);
+    }
+
+    /**
+     * Erase data
+     * @param Fx $f f(x)
+     * @param int $toRemove Direction to remove
+     */
     protected function _eraseData($f, $toRemove) {
         for ($x = $this->_width - 1; 0 <= $x; --$x) {
             if (!isset($this->_map[$x])) {
@@ -214,25 +307,42 @@ class Map {
 
             //$mapX =& $this->_map[$x];
 
-            $limitY = $f($x);
+            $limitY = $f->getY($x);
             _d('$f(' . $x . ') = ' . $limitY);
             if (self::UP === $toRemove) {
-                for ($y = max(array_keys($this->_map[$x])); $limitY <= $y; --$y) {
+                /*for ($y = max(array_keys($this->_map[$x])); $limitY <= $y; --$y) {
                     unset($this->_map[$x][$y]);
+                }*/
+                if ($this->_map[$x]['max'] <= $limitY) {
+                    continue;
                 }
+                if ($limitY < $this->_map[$x]['min']) {
+                    unset($this->_map[$x]);
+                    continue;
+                }
+                $this->_map[$x]['max'] = floor($limitY);
             } elseif (self::DOWN === $toRemove) {
-                for ($y = min(array_keys($this->_map[$x])); $y <= $limitY; ++$y) {
+                /*for ($y = min(array_keys($this->_map[$x])); $y <= $limitY; ++$y) {
                     unset($this->_map[$x][$y]);
+                }*/
+                if ($limitY <= $this->_map[$x]['min']) {
+                    continue;
                 }
+                if ($this->_map[$x]['max'] < $limitY) {
+                    unset($this->_map[$x]);
+                    continue;
+                }
+                $this->_map[$x]['min'] = ceil($limitY);
             } elseif (self::NOT_LIMIT === $toRemove) {
                 //If $x $limitY has already been deleted, remove the whole column.
-                if (!isset($this->_map[$x][$limitY])) {
+                //if (!isset($this->_map[$x][$limitY])) {
+                if ($limitY < $this->_map[$x]['min'] || $this->_map[$x]['max'] < $limitY) {
                     unset($this->_map[$x]);
                     continue;
                 }
 
                 //If not, keep only $x $limitY
-                $this->_map[$x] = array($limitY => true);
+                $this->_map[$x] = array('min' => $limitY, 'max' => $limitY);
             } else {
                 die('ERROR line ' . __LINE__ . ': ' . $toRemove);
             }
