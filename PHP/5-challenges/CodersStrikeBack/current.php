1e7e744838f8ee7ca25b1cfb1fe21bdddebb2928
Hugues Peccatte <hugues.peccatte@aareon.fr>
2019-12-27 01:41:13 +0100

    2ème de la ligue Argent

--- PHP/5-challenges/CodersStrikeBack/current.php
+++ PHP/5-challenges/CodersStrikeBack/current.php
@@ -32,6 +32,8 @@ final class Coordinates
 
 interface PodInterface
 {
+    public function __construct(DistanceManagerInterface $distanceManager);
+
     public function setCurrentCoordinates(Coordinates $coordinates): self;
 
     /**
@@ -39,18 +41,32 @@ interface PodInterface
      * @throws RuntimeException If current coordinates not yet initialized.
      */
     public function getCoordinates(): Coordinates;
+
+    public function calculateNextCoordinates(int $step): Coordinates;
+
     public function getMovementAngle(): ?int;
+
+    public function getSpeed(): int;
+
     public function setNextCheckpointCoordinates(Coordinates $coordinates): self;
+
     public function getNextCheckpointCoordinates(): Coordinates;
+
     public function getDistanceToNextCheckpoint(): int;
+
     public function setDistanceToNextCheckpoint(int $distance): self;
+
     public function getPodAngleWithNextCheckpoint(): int;
+
     public function setPodAngleWithNextCheckpoint(int $angle): self;
 }
 
 final class Pod implements PodInterface
 {
     use angleTrait;
+
+    /** @var DistanceManagerInterface */
+    private $distanceManager;
 
     /** @var Coordinates */
     private $currentCoordinates;
@@ -65,6 +81,11 @@ final class Pod implements PodInterface
     /** @var int */
     private $movementAngle;
 
+    public function __construct(DistanceManagerInterface $distanceManager)
+    {
+        $this->distanceManager = $distanceManager;
+    }
+
     public function setCurrentCoordinates(Coordinates $coordinates): PodInterface
     {
         $this->previousCoordinates = $this->currentCoordinates;
@@ -76,6 +97,39 @@ final class Pod implements PodInterface
     public function getCoordinates(): Coordinates
     {
         return clone($this->currentCoordinates);
+    }
+
+    public function calculateNextCoordinates(int $step, int $power = 100): Coordinates
+    {
+        if (null === $this->previousCoordinates || null === $this->currentCoordinates) {
+            throw new RuntimeException('Not enough data do calculate next coordinates.');
+        }
+
+        if (0 >= $step) {
+            throw new InvalidArgumentException('Step should be > 0.');
+        }
+
+        $movementAngle = $this->getMovementAngle();
+        $speed = $this->getSpeed();
+
+        return $this->calculateCoordinates($this->currentCoordinates, $movementAngle, $speed, $step, $power);
+    }
+
+    private function calculateCoordinates(Coordinates $point, int $movementAngle, int $speed, int $step, int $power = 100): Coordinates
+    {
+        if ($step === 0) {
+            return $point;
+        }
+
+        $newSpeed = $speed*0.85 + $power;
+        _('new speed: ' . $newSpeed);
+        _('Decalage X: ' . cos(deg2rad($movementAngle))*$newSpeed);
+        _('Decalage Y: ' . sin(deg2rad($movementAngle))*$newSpeed);
+
+        return $this->calculateCoordinates(new Coordinates(
+            round($point->getX() + cos(deg2rad($movementAngle))*$newSpeed),
+            round($point->getY() + sin(deg2rad($movementAngle))*$newSpeed)
+        ), $movementAngle, round($newSpeed), --$step, $power);
     }
 
     private function getPreviousCoordinates(): Coordinates
@@ -141,6 +195,15 @@ final class Pod implements PodInterface
         }
 
         return $this->movementAngle;
+    }
+
+    public function getSpeed(): int
+    {
+        try {
+            return $this->distanceManager->measure($this->getPreviousCoordinates(), $this->getCoordinates());
+        } catch (RuntimeException $e) {
+            return 0;
+        }
     }
 }
 
@@ -218,7 +281,7 @@ final class CheckpointsManager implements CheckpointsManagerInterface
 
 interface RunStrategyInterface
 {
-    public function __construct(PodInterface $ally, PodInterface $enemy, CheckpointsManagerInterface $manager);
+    public function __construct(PodInterface $ally, PodInterface $enemy, CheckpointsManagerInterface $manager, DistanceManagerInterface $distanceManager);
 
     /**
      * TODO Replace output by an object.
@@ -269,6 +332,21 @@ trait angleTrait
     }
 }
 
+interface DistanceManagerInterface
+{
+    public function measure(Coordinates $p1, Coordinates $p2): int;
+}
+
+final class DistanceManager implements DistanceManagerInterface
+{
+    public function measure(Coordinates $p1, Coordinates $p2): int
+    {
+        $abscisse = $p1->getX() - $p2->getX();
+        $ordonnee = $p1->getY() - $p2->getY();
+        return round(sqrt(($abscisse ** 2) + ($ordonnee ** 2)));
+    }
+}
+
 final class DummyRunStrategy implements RunStrategyInterface
 {
     use angleTrait;
@@ -282,20 +360,35 @@ final class DummyRunStrategy implements RunStrategyInterface
     /** @var CheckpointsManagerInterface */
     private $checkpointManager;
 
-    public function __construct(PodInterface $ally, PodInterface $enemy, CheckpointsManagerInterface $manager)
+    /** @var DistanceManagerInterface */
+    private $distanceManager;
+
+    /** @var int Last time the shield was activated */
+    private $lastShield = 4;
+
+    public function __construct(PodInterface $ally, PodInterface $enemy, CheckpointsManagerInterface $manager, DistanceManagerInterface $distanceManager)
     {
         $this->ally = $ally;
         $this->enemy = $enemy;
         $this->checkpointManager = $manager;
+        $this->distanceManager = $distanceManager;
     }
 
     public function getDestination(): array
     {
+        ++$this->lastShield;
         $thrust = null;
         $allyCoordinates = $this->ally->getCoordinates();
         $checkpointCoordinates = $this->ally->getNextCheckpointCoordinates();
         $myDist = $this->ally->getDistanceToNextCheckpoint();
         $myAngleToCheckpoint = $this->ally->getPodAngleWithNextCheckpoint();
+
+        $currentMovementAngle = $this->ally->getMovementAngle();
+        $allySpeed = $this->ally->getSpeed();
+        $enemySpeed = $this->enemy->getSpeed();
+        _('SPEED: ' . $allySpeed);
+        $angleToReachCheckpoint = $this->getAngle($allyCoordinates, $checkpointCoordinates);
+        $myAngle = $this->addAngles($angleToReachCheckpoint, $myAngleToCheckpoint);
 
         $abscisse = $checkpointCoordinates->getX() - $allyCoordinates->getX();
         $ordonnee = $checkpointCoordinates->getY() - $allyCoordinates->getY();
@@ -304,48 +397,99 @@ final class DummyRunStrategy implements RunStrategyInterface
         $destAbscisse = $abscisse * $facteur;
         $destOrdonnee = $ordonnee * $facteur;
 
+        try {
+            if ($this->lastShield < 3) {
+                throw new RuntimeException('Shield activated ' . $this->lastShield . ' turn(s) ago.');
+            }
+            $distBetweenPodsIn1Step = $this->distanceManager->measure(
+                $allyNextCoordinates = $this->ally->calculateNextCoordinates(1),
+                $enemyNextCoordinates = $this->enemy->calculateNextCoordinates(1)
+            );
+            _('Ally next coordinates: ');
+            _($allyNextCoordinates);
+            _('Enemy next coordinates: ');
+            _($enemyNextCoordinates);
+            $ecartPodsMovementAngle = $this->diffAngles($currentMovementAngle, $this->enemy->getMovementAngle());
+            $anglePods = $this->getAngle($this->enemy->getCoordinates(), $this->ally->getCoordinates());
+            $ecartPodsAngleMyAngle = $this->diffAngles($anglePods, $myAngle);
+            $enemyIsBehindAlly = abs($ecartPodsAngleMyAngle) < 45;
+            $ecartReversePodsAngleMyAngle = $this->diffAngles($this->addAngles($anglePods, 180), $myAngle);
+            $allyIsBehindEnemy = abs($ecartReversePodsAngleMyAngle) < 45;
+            _('$allySpeed: ' . $allySpeed);
+            _('$enemySpeed: ' . $enemySpeed);
+            _('$ecartPodsAngleMyAngle: ' . $ecartPodsAngleMyAngle);
+            _('$ecartReversePodsAngleMyAngle: ' . $ecartReversePodsAngleMyAngle);
+            _('$ecartPodsMovementAngle: ' . $ecartPodsMovementAngle);
+            _('$enemyIsBehindAlly: ' . (int)$enemyIsBehindAlly);
+            _('$allyIsBehindEnemy: ' . (int)$allyIsBehindEnemy);
+            _('Distance in 1 step: ' . $distBetweenPodsIn1Step);
+            if ($distBetweenPodsIn1Step < 750 && $enemySpeed > 300
+                && (
+                    //
+                    (!$enemyIsBehindAlly && abs($ecartPodsMovementAngle) > 45)
+                    || ($allyIsBehindEnemy && abs($ecartPodsMovementAngle) < 45)
+                )
+            ) {
+                $this->lastShield = 0;
+                return [
+                    'x' => $allyCoordinates->getX() + round($destAbscisse),
+                    'y' => $allyCoordinates->getY() + round($destOrdonnee),
+                    'thrust' => 'SHIELD',
+                ];
+            }
+            $distBetweenPodsIn2Steps = $this->distanceManager->measure(
+                $allyCoordinatesIn2Steps = $this->ally->calculateNextCoordinates(2),
+                $enemyCoordinatesIn2Steps = $this->enemy->calculateNextCoordinates(2)
+            );
+            _('Distance in 2 steps: ' . $distBetweenPodsIn2Steps);
+            if ($distBetweenPodsIn2Steps < 800 && $enemySpeed > 300 && abs($ecartPodsAngleMyAngle) > 45 && abs($ecartPodsMovementAngle) > 45) {
+                _('Will shield on next turn.');
+                //Se tourner vers l'ennemi et accélérer.
+                return [
+                    'x' => $enemyCoordinatesIn2Steps->getX() * 2 - $allyCoordinatesIn2Steps->getX(),
+                    'y' => $enemyCoordinatesIn2Steps->getY() * 2 - $allyCoordinatesIn2Steps->getY(),
+                    'thrust' => '100',
+                ];
+            }
+        } catch (RuntimeException $e) {
+            _($e->getMessage());
+        }
+
         //Si l'angle du mouvement n'est pas assez proche de l'angle nécessaire pour arriver à destination,
         //alors il faut donner au pod un angle pour compenser.
-        $currentMovementAngle = $this->ally->getMovementAngle();
         if (null !== $currentMovementAngle) {
-            $angleToReachCheckpoint = $this->getAngle($allyCoordinates, $checkpointCoordinates);
             $ecartAngleToReachTarget = $this->diffAngles($angleToReachCheckpoint, $currentMovementAngle);
             $angleToTarget = null;
 
-            $myAngle = $this->addAngles($angleToReachCheckpoint, $myAngleToCheckpoint);
-
             if (abs($ecartAngleToReachTarget) > 90) {
-                _('> 90');
-                //Target median between checkpoint and opposite of movement.
-                $oppositeAngleToMovement = $this->addAngles($currentMovementAngle, 180);
-                _('ANGLE OPPOSITE TO MOVEMENT: ' . $oppositeAngleToMovement);
-                $ecartAngleOppositeCheckpoint = $this->diffAngles($angleToReachCheckpoint, $oppositeAngleToMovement);
-                _('ANGLE BETWEEN OPPOSITE AND CP: ' . $ecartAngleOppositeCheckpoint);
-                $angleToTarget = $this->addAngles($oppositeAngleToMovement, (int) ($ecartAngleOppositeCheckpoint / 2));
-                _('PRE-COMPENSATION: ' . $angleToTarget);
+                if ($allySpeed > 200) {
+                    _('> 90');
+                    //Target median between checkpoint and opposite of movement.
+                    $oppositeAngleToMovement = $this->addAngles($currentMovementAngle, 180);
+                    _('ANGLE OPPOSITE TO MOVEMENT: ' . $oppositeAngleToMovement);
+                    $ecartAngleOppositeCheckpoint = $this->diffAngles(
+                        $angleToReachCheckpoint,
+                        $oppositeAngleToMovement
+                    );
+                    _('ANGLE BETWEEN OPPOSITE AND CP: ' . $ecartAngleOppositeCheckpoint);
+                    $angleToTarget = $this->addAngles(
+                        $oppositeAngleToMovement,
+                        (int) ($ecartAngleOppositeCheckpoint / 2)
+                    );
+                    _('PRE-COMPENSATION: ' . $angleToTarget);
 
-                $ecartAnglePodMovement = $this->diffAngles($currentMovementAngle, $myAngle);
-                if ($ecartAnglePodMovement > 0) {
-                    $angleToTarget = $this->addAngles($angleToTarget, +1);
-                } else {
-                    $angleToTarget = $this->addAngles($angleToTarget, -1);
+                    $ecartAnglePodMovement = $this->diffAngles($currentMovementAngle, $myAngle);
+                    if ($ecartAnglePodMovement > 0) {
+                        $angleToTarget = $this->addAngles($angleToTarget, +1);
+                    } else {
+                        $angleToTarget = $this->addAngles($angleToTarget, -1);
+                    }
+                    _('POST-COMPENSATION: ' . $angleToTarget);
                 }
-                _('POST-COMPENSATION: ' . $angleToTarget);
 
-                /*$angleToTarget = $this->addAngles($currentMovementAngle, 180);
-                $ecartAnglePodMovement = $this->diffAngles($currentMovementAngle, $myAngle);
-                _('ANGLE POD->MOVEMENT: ' . $ecartAnglePodMovement);
-                _('PRE-COMPENSATION: ' . $angleToTarget);
-                if ($ecartAnglePodMovement > 0) {
-                    $angleToTarget = $this->addAngles($angleToTarget, -(int)($ecartAnglePodMovement/2)+1);
-                } else {
-                    $angleToTarget = $this->addAngles($angleToTarget, -(int)($ecartAnglePodMovement/2)-1);
-                }
-                _('POST-COMPENSATION: ' . $angleToTarget);
-                $thrust = 100;*/
             } elseif (abs($ecartAngleToReachTarget) > 30) {
                 _('> 30');
-                $angleToTarget = $this->addAngles($angleToReachCheckpoint, $ecartAngleToReachTarget);
+                $angleToTarget = $this->addAngles($angleToReachCheckpoint, round($ecartAngleToReachTarget/2));
             }
 
             _('POD ABSOLUTE ANGLE: ' . $myAngle);
@@ -358,42 +502,54 @@ final class DummyRunStrategy implements RunStrategyInterface
                 $destOrdonnee = sin(deg2rad($angleToTarget)) * $myDist;
                 _('New abscisse: ' . $destAbscisse);
                 _('New ordonnee: ' . $destOrdonnee);
+                //$thrust = 100 - min(50, round(abs($myAngleToCheckpoint/2)));
             }
         }
 
         if (null === $thrust) {
-            if (abs($myAngleToCheckpoint) < 15 && $myDist > 5000) {
+            if (abs($myAngleToCheckpoint) < 15 && $myDist > 6000) {
                 $thrust = 'BOOST';
             } elseif (abs($myAngleToCheckpoint) > 135) {
-                $thrust = 0;
+                $thrust = 10;
             } elseif (abs($myAngleToCheckpoint) > 90) {
-                $thrust = 15;
+                $thrust = 35;
             } elseif (abs($myAngleToCheckpoint) > 70) {
-                $thrust = 30;
+                $thrust = 60;
+            } elseif ($allySpeed < 250) {
+                $thrust = 100;
             } else {
                 /*if ($myDist < 1500) {
                     $thrust = 30;
-                } else*/if ($myDist < 1500) {
+                } else*/
+                if ($myDist < 1500) {
                     $thrust = 45;
                 } elseif ($myDist < 2000) {
-                    $thrust = 70;
-                } elseif ($myDist < 2500) {
-                    $thrust = 85;
+                    $thrust = 60;
                 } else {
                     $thrust = 100;
                 }
             }
         }
 
-        return ['x' => $allyCoordinates->getX() + round($destAbscisse), 'y' => $allyCoordinates->getY() + round($destOrdonnee), 'thrust' => $thrust];
+        return [
+            'x' => $allyCoordinates->getX() + round($destAbscisse),
+            'y' => $allyCoordinates->getY() + round($destOrdonnee),
+            'thrust' => $thrust,
+        ];
     }
 }
 
-$map = new DummyRunStrategy($ally = new Pod(), $enemy = new Pod(), $manager = new CheckpointsManager());
+
+$distanceManager = new DistanceManager();
+$map = new DummyRunStrategy(
+    $ally = new Pod($distanceManager),
+    $enemy = new Pod($distanceManager),
+    $manager = new CheckpointsManager(),
+    $distanceManager
+);
 
 // game loop
-while (TRUE)
-{
+while (true) {
     fscanf(
         STDIN,
         '%d %d %d %d %d %d',
@@ -425,5 +581,16 @@ while (TRUE)
     _('Distance: ' . $nextCheckpointDist);
     _('Angle: ' . $nextCheckpointAngle);
 
-    echo ($dest['x'] . ' ' . $dest['y'] . ' ' . $dest['thrust'] . ' ' . $nextCheckpointDist . ' ' . $nextCheckpointAngle . ' ' . $dest['thrust'] . "\n");
+    echo($dest['x']
+        . ' '
+        . $dest['y']
+        . ' '
+        . $dest['thrust']
+        . ' '
+        . $nextCheckpointDist
+        . ' '
+        . $nextCheckpointAngle
+        . ' '
+        . $dest['thrust']
+        . "\n");
 }
